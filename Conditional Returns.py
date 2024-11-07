bdcc41ad2d9479d6538b51c453f59722b187a466
vanshika2706 <vanshika.malhotra2706@gmail.com>
2024-11-07 22:13:05 +0530

    Delete Conditional Returns.py

--- Conditional Returns.py
+++
@@ -1,8 +0,0 @@
-def check_even_odd(number):
-    if number % 2 == 0:
-        return "Even"
-    else:return "Odd"
-
-result = check_even_odd(4)
-print(result)
-print("WRITTEN BY VANSHIKA MALHOTRA ERP :- 0221BCA137")
